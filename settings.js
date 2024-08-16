d36cae6f3657556584b0ea0b50c948625a2ba7c9
Zidi683 <zidimalik012@gmail.com>
2024-08-16 08:54:05 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "48699643412"
+global.ownernomer = "923335851481"
 global.ownername = "🐛 MR WASI "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '48699643412'  //creator number
+global.ownernumber = '923335851481'  //creator number
 global.ownername = ' MR WASI ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
